0c0a9173141ece86285458bfdcb217e696c7166d
William Lin <willlin1996@gmail.com>
2020-02-23 16:55:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameter
-  if !hash[key]
-    hash[key] = 1 
-  end
+  hash[key] 
 end
 
 def update_counting_hash(hash, key)
